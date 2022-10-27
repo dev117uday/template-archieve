c1b8a11e3794a0ec5fcc02002888182e97bccb52
Uday Yadav <dev117uday@gmail.com>
2022-10-27 11:37:40 +0530

    cleanup

--- svelte-auth-router-template/src/routes/Home.svelte
+++
@@ -1,3 +0,0 @@
-<div class="container">
-    <button	class="btn btn-primary" href="#/auth/12">Authentication</button>
-</div>
--- svelte-auth-router-template/src/routes/Route.svelte
+++
@@ -1,5 +0,0 @@
-<!-- <script>
-    export let params = {};
-    localStorage.setItem("jwt",params.jwt)
-    window.location.href = "/"
-</script> -->
